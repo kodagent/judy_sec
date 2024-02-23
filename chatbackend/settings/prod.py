b76b5ae20484e5d2d24830308015103d66a88e7e
Ugochukwu Nwachukwu <danielnwachukwu5738@gmail.com>
2024-02-23 04:48:40 +0100

    Update: Added sqlite db

--- chatbackend/settings/prod.py
+++ chatbackend/settings/prod.py
@@ -7,8 +7,12 @@ ALLOWED_HOSTS = ["*"]
 
 
 # ================================ DATABASES =======================================
-DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
-# DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
+# DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
+# # DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
+
+DATABASES = {
+    "default": dj_database_url.config(default="sqlite:///db.sqlite3", conn_max_age=600)
+}
 # ================================ DATABASES =======================================
 
 

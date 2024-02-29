74e8656a299be177f88b8cb79c22ccc257fb6113
Ugochukwu Nwachukwu <danielnwachukwu5738@gmail.com>
2024-02-29 20:17:08 +0100

    Update: added websocket apis for optimization

--- assistant/routing.py
+++ assistant/routing.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from optimizerconsumers import OptimizationConsumer
 
 from assistant import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/', consumers.ChatConsumer.as_asgi()),
+    path("ws/chat/", consumers.ChatConsumer.as_asgi()),
+    # Optimizers
+    path("ws/optimize/", OptimizationConsumer.as_asgi()),
 ]
